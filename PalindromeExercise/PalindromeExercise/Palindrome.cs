c89a0e6ff401a46a1f5db081178bf6c169ec48aa
Jared Lewis <jlewis8493@gmail.com>
2022-02-04 02:15:20 -0500

    PalindromeExercise tests successful

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = word.Reverse().ToString();
+
+            if(reversed.ToString() == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", false)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", false)]
+        public void MyTest(string word, bool expected)
         {
+            //arrange
+            var tester = new WordSmith();
 
+            //act
+            var actual = tester.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
